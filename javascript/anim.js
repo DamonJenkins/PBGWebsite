3fc9e60666e298bdb3dcc11c79094e46a5309add
Kamakaze0001 <dj.rockster@hotmail.com>
2015-05-08 01:07:19 +1200

    Title Animation JS

--- javascript/anim.js
+++ javascript/anim.js
@@ -1,9 +1,9 @@
 // JavaScript Document
 
-//Animates section 1 of mainpage title
-function tsec1Animf(){
-    $("#tsec1").css({
-        "font-family" : "Lucida Console",
+function startup(){
+	setTimeout(function(){
+	$("#tsec1").css({
+		"font-family" : "Lucida Console",
         "font-weight" : "bold",
         "text-align" : "center",
         "margin-bottom" : "12px",
@@ -12,5 +12,92 @@ function tsec1Animf(){
         "font-size" : "75px",
         "text-shadow" : "0px 1px 0px #002eb8,0px 2px 0px #0029a3,0px 3px 0px #00248f,0px 4px 0px #001f7a,0px 5px 0px #001a66,0px 6px 1px rgba(0,0,0,.1),0px 0px 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4), 0 3px 5px rgba(0,0,0,.5), 0 5px 10px rgba(0,0,0,.8), 0 10px 10px rgba(0,0,0,.6), 0 20px 20px rgba(0,0,0,.75)",
         "margin-top" : "15px"
-        });
+	});
+	
+	$("#tsec1").hover(function(){
+		$("#tsec1").css({
+		"color" : "#0066ff",
+		"text-shadow" : "0 1px 0 #005ce6,0 2px 0 #0052cc,0 3px 0 #0047b2,0 4px 0 #003d99,0 5px 0 #003380,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4),0 3px 5px rgba(0,0,0,.50),0 5px 10px rgba(0,0,0,.80),0 10px 10px rgba(0,0,0,.60),0 20px 20px rgba(0,0,0,.75)"
+	})}, function(){
+	$("#tsec1").css({
+		"font-family" : "Lucida Console",
+        "font-weight" : "bold",
+        "text-align" : "center",
+        "margin-bottom" : "12px",
+        "top" : "0px",
+        "color" : "#0033cc",
+        "font-size" : "75px",
+        "text-shadow" : "0px 1px 0px #002eb8,0px 2px 0px #0029a3,0px 3px 0px #00248f,0px 4px 0px #001f7a,0px 5px 0px #001a66,0px 6px 1px rgba(0,0,0,.1),0px 0px 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4), 0 3px 5px rgba(0,0,0,.5), 0 5px 10px rgba(0,0,0,.8), 0 10px 10px rgba(0,0,0,.6), 0 20px 20px rgba(0,0,0,.75)",
+        "margin-top" : "15px"
+		});
+	})
+	//Next
+		$("#tsec2").css({
+			"color" : "#009900",
+			"text-shadow" : "0 1px 0 #008a00,0 2px 0 #007a00,0 3px 0 #006b00,0 4px 0 #005c00,0 5px 0 #004c00,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4),0 3px 5px rgba(0,0,0,.50),0 5px 10px rgba(0,0,0,.80),0 10px 10px rgba(0,0,0,.60),0 20px 20px rgba(0,0,0,.75)",
+        "margin-top" : "15px"
+	});
+	
+	$("#tsec2").hover(function(){
+		$("#tsec2").css({
+ 		"color" : "#00cc00",
+		"text-shadow" : "0 1px 0 #00b800,0 2px 0 #00a300,0 3px 0 #008f00,0 4px 0 #007a00,0 5px 0 #006600,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4),0 3px 5px rgba(0,0,0,.50),0 5px 10px rgba(0,0,0,.80),0 10px 10px rgba(0,0,0,.60),0 20px 20px rgba(0,0,0,.75)"
+	})}, function(){
+	$("#tsec2").css({
+			"color" : "#009900",
+			"text-shadow" : "0 1px 0 #008a00,0 2px 0 #007a00,0 3px 0 #006b00,0 4px 0 #005c00,0 5px 0 #004c00,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4),0 3px 5px rgba(0,0,0,.50),0 5px 10px rgba(0,0,0,.80),0 10px 10px rgba(0,0,0,.60),0 20px 20px rgba(0,0,0,.75)",
+        "margin-top" : "15px"
+		});
+	})
+	//Next
+			$("#tsec3").css({
+			 	"color" : "#9900cc",
+	"text-shadow" : "0 1px 0 #8a00b8,0 2px 0 #7a00a3,0 3px 0 #6b008f,0 4px 0 #5c007a,0 5px 0 #4c0066,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4),0 3px 5px rgba(0,0,0,.50),0 5px 10px rgba(0,0,0,.80),0 10px 10px rgba(0,0,0,.60),0 20px 20px rgba(0,0,0,.75)"
+	});
+	
+	$("#tsec3").hover(function(){
+		$("#tsec3").css({
+ 		 	"color" : "#cc00cc",
+	"text-shadow" : "0 1px 0 #b800b8, 0 2px 0 #a300a3,0 3px 0 #8f008f,0 4px 0 #7a007a,0 5px 0 #660066,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4),0 3px 5px rgba(0,0,0,.50),0 5px 10px rgba(0,0,0,.80),0 10px 10px rgba(0,0,0,.60),0 20px 20px rgba(0,0,0,.75)"
+	})}, function(){
+	$("#tsec3").css({
+		"color" : "#9900cc",
+	"text-shadow" : "0 1px 0 #8a00b8,0 2px 0 #7a00a3,0 3px 0 #6b008f,0 4px 0 #5c007a,0 5px 0 #4c0066,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4),0 3px 5px rgba(0,0,0,.50),0 5px 10px rgba(0,0,0,.80),0 10px 10px rgba(0,0,0,.60),0 20px 20px rgba(0,0,0,.75)"
+		});
+	})
+	//Next
+		$("#tsec4").css({
+ 	"color" : "#cc3300",
+	"text-shadow" : "0 1px 0 #b82e00,0 2px 0 #a32900,0 3px 0 #8f2400,0 4px 0 #7a1f00,0 5px 0 #661a00,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4),0 3px 5px rgba(0,0,0,.50),0 5px 10px rgba(0,0,0,.80),0 10px 10px rgba(0,0,0,.60),0 20px 20px rgba(0,0,0,.75)"
+	});
+	
+	$("#tsec4").hover(function(){
+		$("#tsec4").css({
+ 		 	"color" : "#ff6600",
+			"text-shadow" : "0 1px 0 #e65c00,0 2px 0 #cc5200,0 3px 0 #b24700,0 4px 0 #993d00,0 5px 0 #803300,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4),0 3px 5px rgba(0,0,0,.50),0 5px 10px rgba(0,0,0,.80),0 10px 10px rgba(0,0,0,.60),0 20px 20px rgba(0,0,0,.75)"
+	})}, function(){
+	$("#tsec4").css({
+ 	"color" : "#cc3300",
+	"text-shadow" : "0 1px 0 #b82e00,0 2px 0 #a32900,0 3px 0 #8f2400,0 4px 0 #7a1f00,0 5px 0 #661a00,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4),0 3px 5px rgba(0,0,0,.50),0 5px 10px rgba(0,0,0,.80),0 10px 10px rgba(0,0,0,.60),0 20px 20px rgba(0,0,0,.75)"
+		});
+	})
+	//Next
+			$("#tsec5").css({
+		 	 	"color" : "#FF3399",
+	"text-shadow" : "0 1px 0 #e62e8a,0 2px 0 #cc297a,0 3px 0 #b2246b,0 4px 0 #991f5c,0 5px 0 #801a4c,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4),0 3px 5px rgba(0,0,0,.50),0 5px 10px rgba(0,0,0,.80),0 10px 10px rgba(0,0,0,.60),0 20px 20px rgba(0,0,0,.75)"
+	});
+	
+	$("#tsec5").hover(function(){
+		$("#tsec5").css({
+ 		 	"color" : "#ff70b8",
+	"text-shadow" : "0 1px 0 #E665A6,0 2px 0 #CC5A93,0 3px 0 #B24E81,0 4px 0 #99436E,0 5px 0 #80385C,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4),0 3px 5px rgba(0,0,0,.50),0 5px 10px rgba(0,0,0,.80),0 10px 10px rgba(0,0,0,.60),0 20px 20px rgba(0,0,0,.75)"
+	})}, function(){
+	$("#tsec5").css({
+		 "color" : "#FF3399",
+		"text-shadow" : "0 1px 0 #e62e8a,0 2px 0 #cc297a,0 3px 0 #b2246b,0 4px 0 #991f5c,0 5px 0 #801a4c,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.4),0 3px 5px rgba(0,0,0,.50),0 5px 10px rgba(0,0,0,.80),0 10px 10px rgba(0,0,0,.60),0 20px 20px rgba(0,0,0,.75)"
+		});
+	})
+	//Next
+	
+}, 4500);
 }
